f8ffe5d5be017a91f8ff5a62fb11a7ac44afae9b
Isaac Ngeno <isaac2ngeno5@gmail.com>
2018-12-16 18:26:01 +0300

    create issue is complete

--- index.php
+++ index.php
@@ -4,13 +4,120 @@
  * @author : IsaacNgeno
  * Date: 12/15/18
  */
-
+require_once('config/database.php');
 session_start();
 
-if (!isset($_SESSION['user'])){
+if (!isset($_SESSION['user'])) {
     header('Location: pages/logout.php');
     exit;
-}else{
+} else {
     $user = $_SESSION['user'];
-    echo "Welcome ".$user;
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Title</title>
+
+    <link rel="stylesheet" type="text/css" href="fonts/roboto/roboto.css">
+    <link rel="stylesheet" type="text/css" href="fonts/material/material-icons.css">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <style>
+        #index .col-lg-4{
+            color: #FFFFFF;
+        }
+
+    </style>
+</head>
+<body id="index">
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="collapsed navbar-toggle" data-toggle="collapse"
+                    data-target="#navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">Trello</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <p class="navbar-text navbar-right">
+                Signed in as <a href="#" class="navbar-link"><?= $user ?></a>
+            </p>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.php"><i class="material-icons">home</i></a></li>
+                <li><a href="#createIssue"><button type="button" class="btn btn-success" data-toggle="modal" data-target="#createIssue">Create Issue</button></a></li>
+                <li><a href="pages/logout.php"><i class="material-icons">exit_to_app</i></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<div class="container">
+
+    <!-- CONTENT HERE -->
+    <div class="row text-center">
+        <div class="col-lg-4 bg-primary"><h3>In Que</h3></div>
+        <div class="col-lg-4 bg-info"><h3>Running</h3></div>
+        <div class="col-lg-4 bg-success"><h3>Complete</h3></div>
+    </div>
+
+    <!-- Modal -->
+    <div id="createIssue" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Create Issue</h4>
+                </div>
+                <div class="modal-body">
+                    <form action="index.php" method="post" class="form-horizontal">
+                        <input type="hidden" name="user" value="<?= $user ?>">
+                        <input type="text" class="form-control" name="title" placeholder="Title">
+                        <textarea name="description" id="description" class="form-control" cols="30" rows="10" placeholder="Description"></textarea>
+                        <button type="submit" name="create" class="btn btn-success form-control">Create Issue</button>
+                    </form>
+                </div>
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
+<?php
+$db = new Database();
+$pdo = $db->getConnection();
+
+if (isset($_POST['create'])){
+    $title = $_POST['title'];
+    $username = $_POST['user'];
+    $description = $_POST['description'];
+
+    if (!empty($title) && !empty($username) && !empty($description)){
+        $statement = $pdo->prepare("INSERT INTO `issues`( `title`, `description`, `regno`, `status`) VALUES (?, ?, ?, ?)");
+        if ($statement->execute(array($title, $description, $username, "in_que"))){
+            header("Location: index.php");
+        }
+    }
+}
+
